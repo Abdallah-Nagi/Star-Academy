06b4e2ee7cd7d9f7de11bf0fedc51a85f56182e6
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2023-04-14 15:41:54 +0200

    edit courses section

--- src/Home/Courses/Courses.js
+++ src/Home/Courses/Courses.js
@@ -1,78 +1,75 @@
 import React from "react";
 import "./Courses.css";
+import biologyImage from "../../../src/assets/biology.jpg";
+import profile from "../../../src/assets/profile.jpg";
+
 function Courses() {
   return (
     <section className="courses">
       <h2>Avaliable Courses</h2>
       <div className="courses-container">
         <div className="course">
-          <div className="course-main">
-            <div className="course-info">
-              <span className="desc">Course</span>
-              <h4>Biology Cambridge O.L</h4>
-              <a href="#">View course</a>
+          <a href="#"></a>
+          <img className="course-image" src={biologyImage} />
+          <div className="course-info">
+            <h4 className="title">Biology Cambridge O.L</h4>
+            <div className="tutor">
+              <img src={profile} className="image" />
+              <span className="name">Abdallah Nagy</span>
             </div>
-            <div className="course-tutor">
-              <img src="/images/profile.jpg" className="img-tutor" />
-              <span className="name-tutor">Abdallah Nagy</span>
-            </div>
-          </div>
-          <div className="course-description">
-            <span className="desc">INCLUDES</span>
-            <ul>
-              <li>Cover all of the IGCSE Biology curriculum</li>
-              <li>Classified questions for each topic</li>
-              <li>Flash Cards for each topic</li>
-              <li>Previous exams solving from previous 10+ years</li>
-            </ul>
-            <div className="price-container">
-              <span className="price-desc">price:</span>
-              <span className="price">5000.00 EGP</span>
-            </div>
-            <div className="course-btn-container btn-container">
-              <a className="course-btn btn" href="#">
-                Buy now
-              </a>
-              <a className="course-btn btn" href="#">
-                Add to cart
-              </a>
-            </div>
+            <span className="price">7500.00 EGP</span>
           </div>
         </div>
-      </div>
-      <div className="courses-container">
+
         <div className="course">
-          <div className="course-main">
-            <div className="course-info">
-              <span className="desc">Course</span>
-              <h4>Human Biology O.L</h4>
-              <a href="#">View course</a>
+          <a href="#"></a>
+          <img className="course-image" src={biologyImage} />
+          <div className="course-info">
+            <h4 className="title">Biology Cambridge O.L</h4>
+            <div className="tutor">
+              <span className="name">Abdallah Nagy</span>
+              <img src="" className="image" />
             </div>
-            <div className="course-tutor">
-              <img src="/images/profile.jpg" className="img-tutor" />
-              <span className="name-tutor">Abdallah Nagy</span>
-            </div>
+            <span className="price">7500.00 EGP</span>
           </div>
-          <div className="course-description">
-            <span className="desc">INCLUDES</span>
-            <ul>
-              <li>Cover all of the IGCSE Biology curriculum</li>
-              <li>Classified questions for each topic</li>
-              <li>Flash Cards for each topic</li>
-              <li>Previous exams solving from previous 10+ years</li>
-            </ul>
-            <div className="price-container">
-              <span className="price-desc">price:</span>
-              <span className="price">5000.00 EGP</span>
+        </div>
+
+        <div className="course">
+          <a href="#"></a>
+          <img className="course-image" src={biologyImage} />
+          <div className="course-info">
+            <h4 className="title">Biology Cambridge O.L</h4>
+            <div className="tutor">
+              <span className="name">Abdallah Nagy</span>
+              <img src="" className="image" />
             </div>
-            <div className="course-btn-container btn-container">
-              <a className="course-btn btn" href="#">
-                Buy now
-              </a>
-              <a className="course-btn btn" href="#">
-                Add to cart
-              </a>
+            <span className="price">7500.00 EGP</span>
+          </div>
+        </div>
+
+        <div className="course">
+          <a href="#"></a>
+          <img className="course-image" src={biologyImage} />
+          <div className="course-info">
+            <h4 className="title">Biology Cambridge O.L</h4>
+            <div className="tutor">
+              <span className="name">Abdallah Nagy</span>
+              <img src="" className="image" />
             </div>
+            <span className="price">7500.00 EGP</span>
+          </div>
+        </div>
+
+        <div className="course">
+          <a href="#"></a>
+          <img className="course-image" src={biologyImage} />
+          <div className="course-info">
+            <h4 className="title">Biology Cambridge O.L</h4>
+            <div className="tutor">
+              <span className="name">Abdallah Nagy</span>
+              <img src="" className="image" />
+            </div>
+            <span className="price">7500.00 EGP</span>
           </div>
         </div>
       </div>
