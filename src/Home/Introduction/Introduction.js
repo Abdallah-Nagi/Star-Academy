6a433270364606cc570081315b0cfdd9936575e9
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2023-04-15 21:08:44 +0200

    add responsive to nav and intro

--- src/Home/Introduction/Introduction.js
+++ src/Home/Introduction/Introduction.js
@@ -1,22 +1,36 @@
 import React from "react";
 import "./Introduction.css";
+import { WiStars } from "react-icons/wi";
 function Introduction() {
   return (
     <section className="introduction">
-      <h1>
-        Your Road to
-        <span className="special-clr"> A*</span>
-        's
-      </h1>
-      <p>
-        Achieve the highest scores via
-        <span> Star Academy</span>. We provide outstanding IGCSE courses in a
-        variety of subjects, our top-notch tutors not only provide solid
-        tutoring of the curriculum but also focus on solving myriad of previous
-        exams questions to ease the exam for our studnets.
-      </p>
-      <div className="btn-container">
-        <a className="btn btn-introduction">View courses</a>
+      <div className="intro-container">
+        <h1>
+          Your Road to
+          <span className="special-clr"> A*</span>
+          's
+        </h1>
+        <p>
+          Achieve the highest scores via
+          <span> Star Academy</span>. We provide outstanding IGCSE courses in a
+          variety of subjects, our top-notch tutors not only provide solid
+          tutoring of the curriculum but also focus on solving myriad of
+          previous exams questions to ease the exam for our studnets.
+        </p>
+        <div className="btn-container">
+          <a className="btn btn-introduction">View courses</a>
+        </div>
+        <div className="star-logo">
+          <div className="star-one">
+            <WiStars />
+          </div>
+          <div className="star-two">
+            <WiStars />
+          </div>
+          <div className="star-three">
+            <WiStars />
+          </div>
+        </div>
       </div>
     </section>
   );
