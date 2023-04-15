3bc5564c788f5f6d3aa7d8c2cb6260112fd03ab6
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2023-04-15 23:02:56 +0200

    responsive css to About

--- src/Home/About/About.js
+++ src/Home/About/About.js
@@ -7,36 +7,38 @@ import { AiFillPieChart, AiOutlineLineChart } from "react-icons/ai";
 function About() {
   return (
     <section className="about">
-      <div className="title-icon">
-        <span className="design-main">
-          <TbCertificate />
-        </span>
-        <span className="design-secondary one">A*</span>
-        <span className="design-secondary two">
-          <GoVerified />
-        </span>
-        <span className="design-secondary three">
-          <AiOutlineLineChart />
-        </span>
-        <span className="design-secondary four">
-          <AiFillPieChart />
-        </span>
-        <span className="design-secondary five"></span>
-      </div>
-      <div className="star-icon"></div>
-      <div className="design-shape"></div>
-      <p className="about-paragraph">
-        Star Academy is built to create well-rounded igcse courses for the
-        students. but unlike any different centre, our tutors focus on creating
-        instructional to the point explanatory lessons of the curriculum. Each
-        course will contain flash cards questions for memorizing high-yield
-        topics, and our tutors will solve past papers questions following each
-        topic to solidify the topics being taught.
-        <span> Try any of our courses free of charge.</span>
-        <div className="btn-container">
-          <a className="btn btn-about">Try Our Courses</a>
+      <div className="about-container">
+        <div className="title-icon">
+          <span className="design-main">
+            <TbCertificate />
+          </span>
+          <span className="design-secondary one">A*</span>
+          <span className="design-secondary two">
+            <GoVerified />
+          </span>
+          <span className="design-secondary three">
+            <AiOutlineLineChart />
+          </span>
+          <span className="design-secondary four">
+            <AiFillPieChart />
+          </span>
+          <span className="design-secondary five"></span>
         </div>
-      </p>
+        <div className="star-icon"></div>
+        <div className="design-shape"></div>
+        <p className="about-paragraph">
+          Star Academy is built to create well-rounded igcse courses for the
+          students. but unlike any different centre, our tutors focus on
+          creating instructional to the point explanatory lessons of the
+          curriculum. Each course will contain flash cards questions for
+          memorizing high-yield topics, and our tutors will solve past papers
+          questions following each topic to solidify the topics being taught.
+          <span> Try any of our courses free of charge.</span>
+          <div className="btn-container">
+            <a className="btn btn-about">Try Our Courses</a>
+          </div>
+        </p>
+      </div>
     </section>
   );
 }
