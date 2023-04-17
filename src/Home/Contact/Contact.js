e80a5d9be294bc1053ec6645fa646c8a3cc30b06
Abdallah Nagy <abdallah.nagy.dev@gmail.com>
2023-04-18 00:11:08 +0200

    edit responsive

--- src/Home/Contact/Contact.js
+++ src/Home/Contact/Contact.js
@@ -4,19 +4,23 @@ import { BsFillPersonFill } from "react-icons/bs";
 function Contact() {
   return (
     <section className="contact">
-      <h2>Have Any Questions?</h2>
-      <p>
-        We welcome any of your inquiries and we are pleased to help you and
-        provide for you easy and updated educational enviroment.
-      </p>
-      <form className="contact-form">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea rows="4" cols="50" placeholder="Message"></textarea>
-        <div className="btn-container">
-          <a className="btn btn-contact">Submit</a>
+      <div className="contact-wrapper">
+        <div className="contact-header">
+          <h2>Have Any Questions?</h2>
+          <p>
+            We welcome any of your inquiries and we are pleased to help you and
+            provide for you easy and updated educational enviroment.
+          </p>
         </div>
-      </form>
+        <form className="contact-form">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea rows="4" cols="50" placeholder="Message"></textarea>
+          <div className="btn-container">
+            <a className="btn btn-contact">Submit</a>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
